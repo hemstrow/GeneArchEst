2920709ceaee135ade33dc3a65776cdb34b69f02
hemstrow <whemstrom@ucdavis.edu>
2020-03-30 23:17:41 -0700

    set number_descriptive_stats to internal

--- R/dataset_documentation.R
+++
@@ -1,3 +0,0 @@
-#' Number of descriptive parameters produced.
-#' @references Hemstrom et al (in prep)
-"number_descriptive_stats"
--- data-raw/number_descriptive_stats.R
+++ data-raw/number_descriptive_stats.R
@@ -1,4 +1,4 @@
 number_descriptive_stats <- 729
 
 # save
-usethis::use_data(number_descriptive_stats, overwrite = TRUE)
+usethis::use_data(number_descriptive_stats, overwrite = TRUE, internal = T)
