cd698f715f6e683629d96cea4de26ab23b342c05
hemstrow <whemstrom@ucdavis.edu>
2020-04-01 19:52:43 -0700

    Added the names of the distribution descriptive stats.

--- data-raw/number_descriptive_stats.R
+++ data-raw/number_descriptive_stats.R
@@ -1,4 +1,12 @@
-number_descriptive_stats <- 729
+# names
+sn <- read.table("data/stat_names.txt")
+sn <- sn[,1]
+sn <- as.character(sn)
+names_descriptive_stats <- sn
+
+usethis::use_data(names_descriptive_stats, overwrite = TRUE)
+
+number_descriptive_stats <- length(names_descriptive_stats)
 
 # save
 usethis::use_data(number_descriptive_stats, overwrite = TRUE, internal = T)
