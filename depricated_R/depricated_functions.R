4cced7abcbe5192c127fc7ba4769aac5dc823634
hemstrow <whemstrom@ucdavis.edu>
2020-04-11 17:43:59 -0700

    Added pi prediction via random forest and scale prediction via gam.

--- depricated_R/depricated_functions.R
+++ depricated_R/depricated_functions.R
@@ -1,31 +1,4 @@
-# function to estimate pi and scale from gmmat results via gam smoothing
-# The difference in the number of peaks
-# seems to relate only to pi, so do that first, then use the fit optimum pi to predict scale, since the two
-# are tightly linked. For the second pass, use only the runs with the best ks scores.
-estimate_pi_scale_from_ABC <- function(x){
 
-  # pi gam
-  res$trans_pi <- log10(1 - res$pi)
-  g <- mgcv::gam(peak_count_diff ~ s(trans_pi, bs = "cs", k = 100), data = res)
-
-  # pred pi gam
-  pd <- data.frame(pi = seq(min(res$pi), max(res$pi), by = .000001))
-  pd$trans_pi <- log10(1 - pd$pi)
-  pd$pred_pcd <- unlist(predict(g, newdata = pd))
-  opt.pi <- pd[which.min(pd$pred_pcd),]$pi
-
-
-  # scale gam
-  ores <- res
-  res <- res[res$ks <= quantile(res$ks, .01),]
-  g2 <- mgcv::gam(scale ~ s(trans_pi, bs = "cs"), data = res)
-
-  # pred scale gam
-  pd2 <- data.frame(trans_pi = log10(1 - opt.pi))
-  opt.scale <- unlist(predict(g2, newdata = pd2))
-
-  return(c(pi = opt.pi, scale = opt.scale))
-}
 
 
 
