3a28d3070f00456790f867c914d514fbed459b00
hemstrow <whemstrom@ucdavis.edu>
2020-09-01 19:03:35 -0700

    updated documentation, added a fixed number bayesB distribution

--- R/distribution_functions.R
+++ R/distribution_functions.R
@@ -1,21 +1,47 @@
 #=======distribution functions=========
-#' Get random draws from the distribution used for bayesB regressions.
+#' Effect size distributions.
 #'
-#' Generate any number of random values drawn from the distribution used for
-#' bayesB genomic regressions.
+#' Generate any number of random values drawn from the locus effect size
+#' distributions.
 #'
+#' @section BayesB:
 #' Under a bayesB model, the effect of each site is drawn from a distribution
-#' where var(g) == 0 with prob \emph{pi}, and is otherwise drawn from a scaled
-#' t distribution with degrees of freedom \emph{d.f} and scale
-#' \emph{scale}.
+#' where var(g) == 0. Each locus has probability \emph{pi} of having no effect. Effects are otherwise drawn from a scaled t
+#' distribution with degrees of freedom \emph{d.f} and scale \emph{scale}.
+#' Alternatively, a fixed number of sites (\emph{sites}) can be given an effect from the same
+#' distribution. This is equivalent to the expected number of sites when \emph{pi} = 1
+#' - (\emph{sites}/\emph{n}), where \emph{n} is the number of loci.
+#'
+#' @section scaled normal:
+#' Effects are drawn from a normal distribution where var(g) == 0.
+#' Each locus has probability \emph{pi} of having no effect. Effects are otherwise drawn from a normal
+#' distribution with mean 0, standard deviation \emph{sd}, and scale \emph{scale}.
+#' Alternatively, a fixed number of sites (\emph{sites}) can be given an effect from the same
+#' distribution. This is equivalent to the expected number of sites when \emph{pi} = 1
+#' - (\emph{sites}/\emph{n}), where \emph{n} is the number of loci.
+#'
+#' @section flat:
+#' Each locus has probability \emph{pi} of having no effect. Effects are otherwise equal to
+#' the parameter scale \emph{scale}.
+#' Alternatively, a fixed number of sites (\emph{sites}) can be given an effect from the same
+#' distribution. This is equivalent to the expected number of sites when \emph{pi} = 1
+#' - (\emph{sites}/\emph{n}), where \emph{n} is the number of loci.
 #'
 #' @param n numeric. Number of draws to make from the distribution
 #' @param pi numeric. Probability that any one site has zero effect
-#' @param d.f numeric. Degrees of freedom for the scaled-inverse chi-squared
-#'   distribution
+#' @param sites numeric. Number of sites that have an effect.
+#' @param d.f numeric. Degrees of freedom for the effect size distribution.
+#' @param sd numeric. Standard deviation for the effect size distribution.
 #' @param scale numeric. Scale/shape parameter for the scaled-inverse
 #'   chi-squared distribution.
+#' @name effect_size_distributions
 #'
+#' @aliases rbayesB rbayesB_fixed rzero_inflated_normal rzero_inflated_normal_fixed rflat rflat_fixed
+NULL
+
+
+
+#' @describeIn effect_size_distributions assign some loci an effect from a scaled t distribution given a probablity of non-zero effects
 #' @export
 rbayesB <- function(n, pi, d.f, scale){
   effects <- rbinom(n, 1, 1 - pi) # these are non-zero
@@ -24,33 +50,45 @@ rbayesB <- function(n, pi, d.f, scale){
   return(effects)
 }
 
+#' @describeIn effect_size_distributions assign a fixed number of loci effects from a scaled t distribution given
 #' @export
-zero_inflated_normal <- function(n){
-  eff <- rbinom(n, 1, prob.effect) #does each site have an effect?
-  eff[which(eff == 1)] <- rnorm(sum(eff), effect.mean, effect.sd) #what are the effect sizes?
-  # (x - min(x))/(max(x) - min(x))
-  return(eff)
-}
-
-#' @export
-fixed_number_normal <- function(n){
+rbayesB_fixed <- function(n, sites, d.f, scale){
   eff <- numeric(n)
-  eff[sample(n, n.eff, replace = F)] <- rnorm(n.eff, effect.mean, effect.sd)
+  eff[sample(n, sites, replace = F)] <- scale * rt(sites, d.f)
+  return(effects)
+}
+
+#' @describeIn effect_size_distributions assign some loci an effect from a scaled normal distribution given a probability of non-zero effects
+#' @export
+rzero_inflated_normal <- function(n, pi, sd, scale){
+  eff <- rbinom(n, 1, 1 - pi) #does each site have an effect?
+  eff[which(eff == 1)] <- scale * rnorm(sum(eff), 0, sd) #what are the effect sizes?
   return(eff)
 }
 
+#' @describeIn effect_size_distributions assign a fixed number of loci effects from a scaled normal distribution
 #' @export
-fixed_number_scaled_t <- function(n, sites, pi, d.f, scale){
-  effects <- numeric(n)
-  effects[sample(n, sites)] <- 1
-  effects[effects == 1] <- scale * rt(sum(effects), d.f)
+rzero_inflated_normal_fixed <- function(n, sites, sd, scale){
+  eff <- numeric(n)
+  eff[sample(n, sites, replace = F)] <- scale * rnorm(sites, 0, sd)
+  return(eff)
+}
+
+#' @describeIn effect_size_distributions asign loci a fixed effect given a probability of non-zero effects
+#' @export
+rflat <- function(n, pi, scale){
+  eff <- rbinom(n, 1, 1 - pi) #does each site have an effect?
+  eff[which(eff == 1)] <- scale
   return(effects)
 }
 
+#' @describeIn effect_size_distributions assign a fixed number of loci a fixed effect
 #' @export
-fixed_number_flat <- function(n, sites, scale){
-  effects <- numeric(n)
-  # effects[sample(n, sites)] <- sample(rep(c(scale, -1 * scale), sites), sites, replace = F) # half are positive effects, half are negative, randomly positioned
-  effects[sample(n, sites)] <- scale
+rflat_fixed <- function(n, sites, scale){
+  eff <- numeric(n)
+  eff[sample(n, sites, replace = F)] <- scale
   return(effects)
 }
+
+
+
